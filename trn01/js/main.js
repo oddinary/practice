f6be8c997308545bf441bb04a5a8c576b08929a2
oddinary <42957439+oddinary@users.noreply.github.com>
2022-05-02 15:14:20 +0900

    Add files via upload

---
+++ trn01/js/main.js
@@ -0,0 +1,14 @@
+//window.addEventListener('DOMContentLoaded', function () {
+//    document.querySelector('.header').style.display = 'none';
+//})
+
+$(function () {
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        // 1000 = 1초
+        vertical: true,
+        dots: true,
+    });
+})
