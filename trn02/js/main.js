dba226910b461d9341ba79e01d057ae75bca1251
oddinary <42957439+oddinary@users.noreply.github.com>
2022-05-09 14:52:04 +0900

    Add files via upload

---
+++ trn02/js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+    //////////////////////////////////////////////////////////////////////////
+
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp();
+    })
+
+    $('.main_slider').on('init reInit afterChange', function () {
+        let current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    })
+
+    $('.main_slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+
+    })
+
+    //////////////////////////////////////////////////////////////////////////
+})
