84bcf9913c32e9e088afbb5efbc739d3bf99e6b2
oddinary <42957439+oddinary@users.noreply.github.com>
2022-05-10 12:35:02 +0900

    Add files via upload

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -19,5 +19,23 @@ $(function () {
 
     })
 
+    $('.movie .dec i:nth-of-type(1)').on('click', function () {
+        $('.movie video').trigger('pause')
+    })
+    $('.movie .dec i:nth-of-type(2)').on('click', function () {
+        $('.movie video').trigger('play')
+    })
+
+    $("#myMovie").YTPlayer({
+        videoURL: 'http://youtu.be/BsekcY04xvQ', containment: '.youtube', autoPlay: true, mute: true, startAt: 0, opacity: 1, showControls: false, playOnlyIfVisible: true
+    });
+
+    $('.youtube i:nth-of-type(1)').on('click', function () {
+        $('#myMovie').YTPPause();
+    })
+    $('.youtube i:nth-of-type(2)').on('click', function () {
+        $('#myMovie').YTPPlay();
+    })
+
     //////////////////////////////////////////////////////////////////////////
 })
