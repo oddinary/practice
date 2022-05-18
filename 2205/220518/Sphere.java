aab80d49e68703ed58f8e9c039f7aa01f056efe9
oddinary <42957439+oddinary@users.noreply.github.com>
2022-05-18 17:19:06 +0900

    Add files via upload

--- 2205/220518/Sphere.java
+++ 2205/220518/Sphere.java
@@ -8,6 +8,9 @@ public class Sphere {
 		double r = input.nextDouble();
 		
 		double volume = (4.0/3.0) * (r * r * r );
-		System.out.print("구의 부피 : " + volume);
+		double result = Math.pow(r,3); // 제곱식
+		System.out.println("구의 부피 : " + volume);
+		System.out.println("구의 부피 : " + 4 * result / 3);
+		
 	}
 }
