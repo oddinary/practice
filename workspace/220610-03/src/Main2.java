3ea86384795cdd4caf612bc62c3502f1208903c6
oddinary <42957439+oddinary@users.noreply.github.com>
2022-06-13 08:48:46 +0900

    Add files via upload

--- workspace/220610-03/src/Main2.java
+++ workspace/220610-03/src/Main2.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 class Can {
 	private String name;
-	
+
 	public Can(String name) {
 		this.name = name;
 	}
+
 	public String toString() {
 		return name;
 	}
@@ -20,78 +21,82 @@ public class Main2 {
 	// 정수형 => 반환 Type : Can
 	// 1 -> field "콜라"
 	// 2 -> field "사이다"
-	
+
 	// 문자열 -> 반환 Type : Can
 	// "Cola" -> field "콜라"
-	
+
+	public static boolean isNumber(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
 	
 	static Can machine(int a) {
 		Can Cola = new Can("콜라");
 		Can Sprite = new Can("사이다");
 		Can Fanta = new Can("환타");
-		Can nothing = new Can("아무것도 없음");
-		
+		Can nothing = new Can("잘못된 입력입니다.");
+
 		switch (a) {
-		case COLA : {
+		case COLA: {
 			return Cola;
 		}
-		case SPRITE : {
+		case SPRITE: {
 			return Sprite;
 		}
-		case FANTA : {
+		case FANTA: {
 			return Fanta;
 		}
-		default : {
+		default: {
 			return nothing;
 		}
 		}
 	}
-	
+
 	static Can machine(String a) {
 		Can Cola = new Can("콜라");
 		Can Sprite = new Can("사이다");
 		Can Fanta = new Can("환타");
-		Can nothing = new Can("아무것도 없음");
-		
+		Can nothing = new Can("잘못된 입력입니다.");
+
 		switch (a) {
-		case "콜라" :
-		case "Cola" :
-		{
+		case "콜라":
+		case "cola":
+		case "Cola": {
 			return Cola;
 		}
-		case "사이다" :
-		case "Sprite" :{
+		case "사이다":
+		case "sprite":
+		case "Sprite": {
 			return Sprite;
 		}
-		case "환타" :
-		case "Fanta" :{
+		case "환타":
+		case "fanta":
+		case "Fanta": {
 			return Fanta;
 		}
-		default : {
+		default: {
 			return nothing;
 		}
 		}
 	}
-	
-	public static void main(String[] args) {
+	static void vending() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("자판기 입력");
-		String number = scan.nextLine();
-//		int num = 0;
-//		if(number.equals("1") || number.equals("2") || number.equals("3"))
-//			{num = Integer.parseInt(number);}
-//		else if (number.equals("Cola") || number.equals("콜라")) {
-//			num = 1;
-//		}
-//		else if (number.equals("Sprite") || number.equals("사이다")) {
-//			num = 2;
-//		}
-//		else if (number.equals("Fanta") || number.equals("환타")) {
-//			num = 3;
-//		}
-//		else {
-//			System.out.println("잘못된 입력입니다.");
-//		}
-		System.out.println(machine(number));
+		System.out.println("1.콜라(Cola), 2.사이다(Sprite), 3.환타(Fanta)");
+		String input = scan.nextLine();
+
+		if (isNumber(input)) {
+			int number = Integer.parseInt(input);
+			System.out.println(machine(number));
+		} else {
+			System.out.println(machine(input));
+		}
+	}
+	public static void main(String[] args) {
+		vending();
 	}
 }
