5f761e137eff8b05d1e4aa71c3b46f4f7a0db989
oddinary <42957439+oddinary@users.noreply.github.com>
2022-06-17 17:44:08 +0900

    Delete workspace/Zoo directory

--- workspace/Zoo/src/ICarnivores.java
+++
@@ -1,6 +0,0 @@
-
-public interface ICarnivores { // 육식동물
-	default void meat() {
-		System.out.println("고기를 먹습니다. 우적우적");
-	}
-}
--- workspace/Zoo/src/IHerbivores.java
+++
@@ -1,8 +0,0 @@
-
-public interface IHerbivores { // 초식동물
-	default void grass() {
-		System.out.println("풀을 먹습니다. 냠냠");
-	}
-}
-
-
